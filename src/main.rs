5a8a1c78f11ec6e683a6cef3ab2dc316287a1a2c
rsookram <rashadsookram@gmail.com>
2021-10-23 11:31:22 -0400

    Propagate errors from git blame

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
+use anyhow::{bail, Result};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use std::{
     collections::BTreeMap,
-    error::Error,
     ffi::{OsStr, OsString},
     process::Command,
 };
 
-fn main() {
+fn main() -> Result<()> {
     let args = std::env::args_os().skip(1).collect::<Vec<_>>();
 
-    let result = run(&args);
+    let result = run(&args)?;
 
     let mut result = result.into_iter().collect::<Vec<_>>();
 
@@ -19,26 +19,36 @@ fn main() {
     for (name, count) in result {
         println!("{}\t{}", count, name);
     }
+
+    Ok(())
 }
 
-fn run(args: &[OsString]) -> BTreeMap<String, usize> {
+fn run(args: &[OsString]) -> Result<BTreeMap<String, usize>> {
     args.par_iter()
-        .map(|arg| count_author_lines(arg).unwrap())
-        .reduce(|| BTreeMap::new(), {
-            |mut a, b| {
-                for (name, count) in b {
-                    *a.entry(name).or_insert(0) += count;
-                }
+        .map(|arg| count_author_lines(arg))
+        .reduce(|| Ok(BTreeMap::new()), {
+            |a, b| match (a, b) {
+                (Ok(mut a), Ok(b)) => {
+                    for (name, count) in b {
+                        *a.entry(name).or_insert(0) += count;
+                    }
 
-                a
+                    Ok(a)
+                }
+                (Err(e), _) => Err(e),
+                (_, Err(e)) => Err(e),
             }
         })
 }
 
-fn count_author_lines(path: &OsString) -> Result<BTreeMap<String, usize>, Box<dyn Error>> {
+fn count_author_lines(path: &OsString) -> Result<BTreeMap<String, usize>> {
     let output = Command::new("git")
         .args(&[OsStr::new("blame"), path, OsStr::new("--line-porcelain")])
         .output()?;
+
+    if !output.status.success() {
+        bail!("{}", String::from_utf8_lossy(&output.stderr));
+    }
 
     let porcelain = std::str::from_utf8(&output.stdout)?;
 
